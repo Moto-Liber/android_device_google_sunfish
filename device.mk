fca72dbe01e511315adeb6b86158ecd569020b8f
Michael Bestas <mikeioannina@gmail.com>
2024-03-08 16:59:21 +0200

    sunfish: Switch to barbet Wi-Fi service
    Change-Id: Ia55f403319252cea93e8bc3786173c5f8d66fb37

--- device.mk
+++ device.mk
@@ -525,12 +525,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0.vendor \
     android.hardware.sensors@2.1.vendor \
     android.hardware.weaver@1.0.vendor:64 \
-    android.hardware.wifi@1.1.vendor:64 \
-    android.hardware.wifi@1.2.vendor:64 \
-    android.hardware.wifi@1.3.vendor:64 \
-    android.hardware.wifi@1.4.vendor:64 \
-    android.hardware.wifi@1.5.vendor:64 \
-    android.hardware.wifi@1.6.vendor:64 \
+    android.hardware.wifi-V2-ndk.vendor:64 \
+    android.hardware.wifi@1.0.vendor:64 \
     android.system.net.netd@1.1.vendor:64
 
 # Properties
